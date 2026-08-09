ff0331b9ef7bfd14b8c5072db613019001ffbfd1
SirCrLs <carlos_leal_04@hotmail.com>
2026-08-08 23:41:30 -0600

    fix: refactorized sync users function

--- etl/sync_data.py
+++ etl/sync_data.py
@@ -168,7 +168,7 @@ def sync_games(conn, api_key, MAX_PAGE, BATCH_SIZE: int = 100):
 
     logger.info("Games sync completed.")
 
-def sync_users(conn, api_key, MAX_USERS:int = 300):
+def sync_users(conn, api_key, MAX_USERS:int = 300, BATCH_SIZE: int = 100):
     """ USERS """
     SEED_FILE: str = os.path.join("..", "data", "seed_steam_ids.txt")
 
@@ -183,43 +183,58 @@ def sync_users(conn, api_key, MAX_USERS:int = 300):
     summaries = clean_summaries(api_key,steam_ids,MAX_USERS)
     logger.info(f"{len(summaries)} summaries loaded.")
 
-    user_rows = [transform_user(player) for player in summaries]
-    loader.upsert_users_batch(conn, user_rows)
-    logger.info(f"{len(user_rows)} users saved into the database.")
-    conn.commit()
+    user_games_batch = []
+    user_achievements_batch = []
 
-    for steam_id in steam_ids:
+    if summaries:
+        user_rows = [transform_user(player) for player in summaries]
+        loader.upsert_users_batch(conn, user_rows)
+        logger.info(f"{len(user_rows)} users saved into the database.")
+        conn.commit()
+
+    for i, steam_id in enumerate(steam_ids, start=1):
         try:
-            logger.info(f"Syncing user {steam_id}...")
+            logger.info(f"Syncing user {steam_id} ({i}/{len(steam_ids)})...")
 
-            # b. Owned games 
-            raw_games = get_owned_games(api_key, steam_id)
-            loader.save_raw_response(conn, "GetOwnedGames", {"steamid": steam_id}, raw_games)
+            # Owned games
+            owned_response = get_owned_games(api_key, steam_id)
+            raw_games = owned_response.get("response", {}).get("games", []) if owned_response else []
+            
+            if raw_games:
+                user_games_rows = transform_owned_games(raw_games,steam_id)
+                user_games_batch.extend(user_games_rows)
 
-            user_games_rows = transform_owned_games(raw_games)
-            loader.upsert_user_games(conn, steam_id, user_games_rows)
-            logger.info(f"{len(user_games_rows)} games kept for user {steam_id}.")
+                game_ids = []
+                for game in user_games_rows:
+                    game_ids.append(game.get("appid"))
 
-            # c. Achievements per owned game
-            for game in user_games_rows:
-                app_id = game["app_id"]
-                try:
-                    raw_ach = get_top_achievements(api_key, steam_id, app_id)
-                    loader.save_raw_response(conn, "GetPlayerAchievements", {"appid": app_id}, raw_ach)
-
-                    ach_rows = transform_user_achievements(raw_ach, steam_id, app_id)
-                    loader.upsert_user_achievements(conn, ach_rows)
-
-                except Exception as e:
-                    logger.debug(f"No achievements for user={steam_id}, app_id={app_id}: {e}")
-                    continue
-
-            conn.commit()
+                # Achievements per owned game
+                achievements = get_top_achievements(api_key, steam_id, game_ids)
+                if achievements: # TO DO: CHANGE TRANSFORM USER ACHIEVEMENTS FUNCTION
+                    ach_rows = transform_user_achievements(achievements, steam_id, game_ids)
+                    user_achievements_batch.extend(ach_rows)
 
         except Exception as e:
-            conn.rollback()
-            logger.error(f"Error syncing user {steam_id}: {e}")
+            logger.warning(f"Error fetching API data for user {steam_id}: {e}")
             continue
+
+        if i % BATCH_SIZE == 0 or i == len(steam_ids):
+            try:
+                if user_games_batch:
+                    loader.upsert_user_games_batch(conn, user_games_batch)
+
+                if user_achievements_batch:
+                    loader.upsert_user_achievements_batch(conn, user_achievements_batch)
+
+                conn.commit()
+                logger.info(f"Batch saved successfully at index {i}/{len(steam_ids)}.")
+
+            except Exception as e:
+                conn.rollback()
+                logger.error(f"Error saving batch at index {i}: {e}. Attempting individual fallback...")
+            finally:
+                user_games_batch.clear()
+                user_achievements_batch.clear()
 
     logger.info("Users sync completed.")
 
