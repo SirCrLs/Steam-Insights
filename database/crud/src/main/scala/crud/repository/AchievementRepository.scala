f34b0dea7af6adf24d82a26dfcd652b230a64d50
SirCrLs <carlos_leal_04@hotmail.com>
2026-08-21 21:58:02 -0600

    minor fix

--- database/crud/src/main/scala/crud/repository/AchievementRepository.scala
+++ database/crud/src/main/scala/crud/repository/AchievementRepository.scala
@@ -6,7 +6,21 @@ import doobie.*
 object AchievementRepository:
   def findAll: ConnectionIO[List[Achievement]] =
     sql"""
-    SELECT
-    FROM achievements
+      SELECT app_id, achievement_key, display_name, achievement_desc, global_unlock_pct
+      FROM achievements
     """.query[Achievement].to[List]
+
+  def findById(appId: Int): ConnectionIO[Option[Achievement]]= 
+    sql"""
+      SELECT app_id, achievement_key, display_name, achievement_desc, global_unlock_pct
+      FROM achievements
+      WHERE app_id = $appId
+    """.query[Achievement].option
+
+  def create(Achievement: Achievement): ConnectionIO[Int] = 
+    sql"""
+      INSERT INTO achievements (app_id, achievement_key, display_name, achievement_desc, global_unlock_pct)
+      VALUES (${Achievement.appId}, ${Achievement.achievementKey}, ${Achievement.displayName},
+        ${Achievement.achievementDesc}, ${Achievement.globalUnlockPct})
+    """ 
 
