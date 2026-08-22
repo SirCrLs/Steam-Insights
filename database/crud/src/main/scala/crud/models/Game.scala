72cca2f9a4389d398ca38d650fa24409927be733
SirCrLs <carlos_leal_04@hotmail.com>
2026-08-21 18:56:27 -0600

    feat: added Game case class

---
+++ database/crud/src/main/scala/crud/models/Game.scala
@@ -0,0 +1,43 @@
+package models
+
+import java.time.{LocalDate, LocalDateTime}
+import io.circe.Codec
+import io.circe.generic.semiauto.deriveCodec
+
+final case class Game(
+  appId: Int,
+  name: String,
+  shortDescription: Option[String],
+  genres: Option[List[String]],
+  categories: Option[List[String]],
+  supportedLanguages: Option[List[String]],
+  headerImage: Option[String],
+  pcRequirementsMinimum: Option[String],
+  pcRequirementsRecommended: Option[String],
+  processor: Option[List[String]],
+  graphics: Option[List[String]],
+  ramRequirement: Option[List[Short]],
+  storageRequirement: Option[List[Short]],
+  developers: Option[String],
+  isOnWindows: Option[Boolean],
+  isOnMac: Option[Boolean],
+  isOnLinux: Option[Boolean],
+  metacriticScore: Option[Short],
+  releaseDate: Option[LocalDate],
+  priceUsd: Option[BigDecimal],
+  isFree: Option[Boolean],
+  rating: Option[String],
+  totalAchievements: Option[Short],
+  recommendations: Option[Int],
+  ownersMin: Option[Long],
+  ownersMax: Option[Long],
+  positiveReviews: Option[Int],
+  negativeReviews: Option[Int],
+  totalReviews: Option[Int],
+  approvalRate: Option[BigDecimal],
+  fetchedAt: Option[LocalDateTime]
+)
+
+object Game {
+  implicit val gameCodec: Codec.AsObject[Game] = deriveCodec[Game]
+}
