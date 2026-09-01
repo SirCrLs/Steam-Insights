f0b6663d9911a21299da0311c6d78eadc3008dc5
SirCrLs <carlos_leal_04@hotmail.com>
2026-08-31 19:03:58 -0600

    changed repository files from objects to classes and fixed minor bugs

--- database/crud/src/main/scala/crud/repository/UserAchievementRepository.scala
+++ database/crud/src/main/scala/crud/repository/UserAchievementRepository.scala
@@ -6,7 +6,7 @@ import doobie.implicits.*
 import doobie.postgres.implicits.*
 import java.time.LocalDate
 
-object UserAchievementRepository:
+class UserAchievementRepository:
 
   def findAllBySteamId(steamId: Long): ConnectionIO[List[UserAchievement]] =
     sql"""
