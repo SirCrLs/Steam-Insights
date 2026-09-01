2be09492f7b7c910edb2f5ebe4fb37277781ebf7
SirCrLs <carlos_leal_04@hotmail.com>
2026-08-31 19:01:17 -0600

    added game routes and modified an object to a class

--- database/crud/src/main/scala/crud/routes/GameRoutes.scala
+++ database/crud/src/main/scala/crud/routes/GameRoutes.scala
@@ -1,2 +1,73 @@
 package routes
 
+import cats.effect.Async
+import cats.syntax.all.*
+import io.circe.syntax.*
+import io.circe.generic.auto.*
+import org.http4s.*
+import org.http4s.dsl.Http4sDsl
+import org.http4s.circe.*
+import org.http4s.server.Router
+import doobie.implicits.toConnectionIOOps
+import doobie.util.transactor.Transactor
+import org.http4s.circe.CirceEntityEncoder.*
+
+import models.Game
+import repository.GameRepository
+
+class GameRoutes[F[_]: Async](gameRepository: GameRepository[F], xa : Transactor[F]) extends Http4sDsl[F]:
+
+  // decode/code JSON 
+  implicit val gameEntityEncoder: EntityEncoder[F, Game] = jsonEncoderOf[F, Game]
+  implicit val gameListEntityEncoder: EntityEncoder[F, List[Game]] = jsonEncoderOf[F, List[Game]]
+  implicit val gameEntityDecoder: EntityDecoder[F, Game] = jsonOf[F, Game]
+
+  val routes: HttpRoutes[F] = HttpRoutes.of[F]:
+
+    // GET all games
+    case GET -> Root => 
+      for
+        games <- gameRepository.findAll.transact(xa)
+        resp <- Ok(games)
+      yield resp
+
+    // GET games by id
+    case GET -> Root / IntVar(id) =>
+      for
+        maybeGame <- gameRepository.findById(id).transact(xa) 
+        resp <- maybeGame match
+          case Some(game) => Ok(game)
+          case None => NotFound(s"Game not found: ID $id")
+      yield resp
+
+    // POST create game
+    case req @ POST -> Root =>
+      for
+        newGame <- req.as[Game]
+        rowsInserted <- gameRepository.create(newGame).transact(xa)
+        resp <- if rowsInserted > 0 then
+          Created(Map("message" -> "Success : ", "count" -> rowsInserted.toString))
+        else
+          BadRequest(Map("error" -> "Create failed"))
+      yield resp
+
+    // PUT update a game
+    case req @ PUT -> Root / IntVar(id) =>
+      for
+        gameUpdate <- req.as[Game]
+        rowsUpdated <- gameRepository.update(id, gameUpdate).transact(xa)
+        resp <- if rowsUpdated > 0 then
+          Ok(Map("message" -> s"Game $id updated."))
+        else
+          NotFound(Map("error" -> s"Update failed: game $id does not exist"))
+      yield resp
+
+    // DELETE game by id
+    case DELETE -> Root / IntVar(id) =>
+      for
+        rowsDeleted <- gameRepository.delete(id).transact(xa)
+        resp <- if rowsDeleted > 0 then
+          NoContent()
+        else
+          NotFound(Map("error" -> s"Delete failed: game $id does not exist"))
+      yield resp
