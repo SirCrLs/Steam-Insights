bddbcd2ae4b8f4bd61181c50aca65cd9ff4af316
SirCrLs <carlos_leal_04@hotmail.com>
2026-09-07 21:12:26 -0600

    fix: fixed a bug where auth-guard didnt work

--- frontend/public/js/auth-guard.js
+++ frontend/public/js/auth-guard.js
@@ -2,11 +2,7 @@
   const isAuthenticated = localStorage.getItem("is_authenticated") === "true";
   const currentPath = window.location.pathname;
 
-  // if not loged and tries to access, redirects to login
   if (!isAuthenticated && !currentPath.endsWith("login.html") && currentPath !== "/") {
     window.location.replace("/login.html");
-  } 
-  else if (isAuthenticated && (currentPath.endsWith("login.html") || currentPath === "/")) {
-    window.location.replace("/index.html");
   }
 })();
