e8ee2ebc8e5a4d54c6ebfb5fa53148e47f8566ab
SirCrLs <carlos_leal_04@hotmail.com>
2026-09-23 20:33:41 -0600

    changed endpoints to use opaque type SteamId

--- database/crud/src/main/scala/crud/routes/UserRoutes.scala
+++ database/crud/src/main/scala/crud/routes/UserRoutes.scala
@@ -13,6 +13,7 @@ import doobie.util.transactor.Transactor
 import org.http4s.circe.CirceEntityEncoder.*
 import org.http4s.circe.CirceEntityDecoder.*
 
+import models.SteamId
 import models.User
 import models.UserGame
 import models.UserAchievement
@@ -44,7 +45,8 @@ class UserRoutes[F[_]: Async](
       yield resp
 
     //GET user by steamid
-    case GET -> Root / LongVar(steamid) =>
+    case GET -> Root / LongVar(rawSteamid) =>
+      val steamid: SteamId = SteamId(rawSteamid)
       userRepository.findById(steamid).transact(xa).attempt.flatMap {
         case Right(Some(user)) => Ok(user)
         case Right(None)       => NotFound(Map("error" -> s"User not found: steamID = $steamid"))
@@ -65,7 +67,8 @@ class UserRoutes[F[_]: Async](
       yield resp
 
     //PUT update a user
-    case req @ PUT -> Root / LongVar(steamid) =>
+    case req @ PUT -> Root / LongVar(rawSteamid) =>
+      val steamid: SteamId = SteamId(rawSteamid)
       for
         userUpdate <- req.as[User]
         rowsUpdated <- userRepository.update(steamid, userUpdate).transact(xa)
@@ -76,7 +79,8 @@ class UserRoutes[F[_]: Async](
       yield resp
 
     // DELETE user by id
-    case DELETE -> Root / LongVar(steamid) =>
+    case DELETE -> Root / LongVar(rawSteamid) =>
+      val steamid: SteamId = SteamId(rawSteamid)
       userRepository.delete(steamid).transact(xa).attempt.flatMap {
         case Right(rowsDeleted) if rowsDeleted > 0 =>
           Ok(Map("message" -> s"User $steamid deleted successfully"))
@@ -100,17 +104,19 @@ class UserRoutes[F[_]: Async](
       yield resp
 
     // GET all games from user
-    case GET -> Root / LongVar(steamId) / GamesURL :? LimitParam(limitOpt) +& OffsetParam(offsetOpt) =>
+    case GET -> Root / LongVar(rawSteamid) / GamesURL :? LimitParam(limitOpt) +& OffsetParam(offsetOpt) =>
+      val steamid: SteamId = SteamId(rawSteamid)
       val limit = limitOpt.getOrElse(100)
       val offset = offsetOpt.getOrElse(0)
       for
-        games <- userGamesRepo.findGamesBySteamId(steamId, limit, offset).transact(xa)
-        total <- userGamesRepo.countBySteamId(steamId).transact(xa)
+        games <- userGamesRepo.findGamesBySteamId(steamid, limit, offset).transact(xa)
+        total <- userGamesRepo.countBySteamId(steamid).transact(xa)
         resp  <- Ok(Map("total" -> total.asJson, "games" -> games.asJson))
       yield resp
 
     // GET one game from user
-    case GET -> Root / LongVar(steamid) / GamesURL / IntVar(appid)=>
+    case GET -> Root / LongVar(rawSteamid) / GamesURL / IntVar(appid)=>
+      val steamid: SteamId = SteamId(rawSteamid)
       for
         maybeGame <- userGamesRepo.findOne(steamid,appid).transact(xa)
         resp <- maybeGame match
@@ -119,24 +125,26 @@ class UserRoutes[F[_]: Async](
       yield resp
 
     // POST upsert a game for user
-    case req @ POST -> Root / LongVar(steamId) / GamesURL =>
+    case req @ POST -> Root / LongVar(rawSteamid) / GamesURL =>
+      val steamid: SteamId = SteamId(rawSteamid)
       for
         userGame <- req.as[UserGame]
         rowsInserted <- userGamesRepo.upsert(userGame).transact(xa)
         resp <- if rowsInserted > 0 then
-          Created(Map("message" -> s"Game upserted for user $steamId successfully"))
+          Created(Map("message" -> s"Game upserted for user $steamid successfully"))
         else
           BadRequest(Map("error" -> "Upsert failed."))
       yield resp
 
     // DELETE a game from user library
-    case DELETE -> Root / LongVar(steamId) / GamesURL / IntVar(appid) =>
-      userGamesRepo.delete(steamId, appid).transact(xa).attempt.flatMap {
+    case DELETE -> Root / LongVar(rawSteamid) / GamesURL / IntVar(appid) =>
+      val steamid: SteamId = SteamId(rawSteamid)
+      userGamesRepo.delete(steamid, appid).transact(xa).attempt.flatMap {
         case Right(rowsDeleted) if rowsDeleted > 0 =>
-          Ok(Map("message" -> s"Game $appid removed from library of user $steamId"))
+          Ok(Map("message" -> s"Game $appid removed from library of user $steamid"))
 
         case Right(_) =>
-          NotFound(Map("error" -> s"Game $appid not found on library of user $steamId"))
+          NotFound(Map("error" -> s"Game $appid not found on library of user $steamid"))
 
         case Left(error) =>
           InternalServerError(Map("error" -> s"Error removing game from library: ${error.getMessage}"))
@@ -155,57 +163,62 @@ class UserRoutes[F[_]: Async](
       yield resp
       
     // GET all achievements from a user
-    case GET -> Root / LongVar(steamId) / AchievementsURL :? LimitParam(limitOpt) +& OffsetParam(offsetOpt) =>
+    case GET -> Root / LongVar(rawSteamid) / AchievementsURL :? LimitParam(limitOpt) +& OffsetParam(offsetOpt) =>
+      val steamid: SteamId = SteamId(rawSteamid)
       val limit = limitOpt.getOrElse(100)
       val offset = offsetOpt.getOrElse(0)
       for
-        achievements <- userAchievementsRepo.findAllBySteamId(steamId, limit, offset).transact(xa)
-        total <- userAchievementsRepo.countBySteamId(steamId).transact(xa)
+        achievements <- userAchievementsRepo.findAllBySteamId(steamid, limit, offset).transact(xa)
+        total <- userAchievementsRepo.countBySteamId(steamid).transact(xa)
         resp <- Ok(Map("total" -> total.asJson, "achievements" -> achievements.asJson))
       yield resp
 
     // GET all achievements of user for a specific game
-    case GET -> Root / LongVar(steamId) / AchievementsURL / IntVar(appid) :? LimitParam(limitOpt) +& OffsetParam(offsetOpt) =>
+    case GET -> Root / LongVar(rawSteamid) / AchievementsURL / IntVar(appid) :? LimitParam(limitOpt) +& OffsetParam(offsetOpt) =>
+      val steamid: SteamId = SteamId(rawSteamid)
       val limit = limitOpt.getOrElse(100)
       val offset = offsetOpt.getOrElse(0)
       for
-        achievements <- userAchievementsRepo.findBySteamIdAndAppId(steamId, appid, limit, offset).transact(xa)
-        total<- userAchievementsRepo.countBySteamIdAndAppId(steamId, appid).transact(xa)
+        achievements <- userAchievementsRepo.findBySteamIdAndAppId(steamid, appid, limit, offset).transact(xa)
+        total<- userAchievementsRepo.countBySteamIdAndAppId(steamid, appid).transact(xa)
         resp <- Ok(Map("total" -> total.asJson, "achievements" -> achievements.asJson))
       yield resp
 
     // PUT Upsert an achievement
-    case req @ PUT -> Root / LongVar(steamId) / AchievementsURL =>
+    case req @ PUT -> Root / LongVar(rawSteamid) / AchievementsURL =>
+      val steamid: SteamId = SteamId(rawSteamid)
       for
         achUpdate   <- req.as[UserAchievement]
         rowsUpdated <- userAchievementsRepo.upsert(achUpdate).transact(xa)
         resp <- if rowsUpdated > 0 then
-          Ok(Map("message" -> s"Achievement upserted successfully for user $steamId"))
+          Ok(Map("message" -> s"Achievement upserted successfully for user $steamid"))
         else
-          BadRequest(Map("error" -> s"Failed to upsert achievement for user $steamId"))
+          BadRequest(Map("error" -> s"Failed to upsert achievement for user $steamid"))
       yield resp
   
     // DELETE all achievements from a game
-    case DELETE -> Root / LongVar(steamId) / AchievementsURL / IntVar(appId) =>
-      userAchievementsRepo.deleteBySteamIdAndAppId(steamId, appId).transact(xa).attempt.flatMap {
+    case DELETE -> Root / LongVar(rawSteamid) / AchievementsURL / IntVar(appId) =>
+      val steamid: SteamId = SteamId(rawSteamid)
+      userAchievementsRepo.deleteBySteamIdAndAppId(steamid, appId).transact(xa).attempt.flatMap {
         case Right(rowsDeleted) if rowsDeleted > 0 =>
-          Ok(Map("message" -> s"All achievements for game $appId removed for user $steamId ($rowsDeleted deleted)"))
+          Ok(Map("message" -> s"All achievements for game $appId removed for user $steamid ($rowsDeleted deleted)"))
 
         case Right(_) =>
-          NotFound(Map("error" -> s"Achievements not found for game $appId and user $steamId"))
+          NotFound(Map("error" -> s"Achievements not found for game $appId and user $steamid"))
 
         case Left(error) =>
           InternalServerError(Map("error" -> s"Error deleting achievements: ${error.getMessage}"))
       }
 
     // DELETE one specific achievement
-    case DELETE -> Root / LongVar(steamId) / AchievementsURL / IntVar(appId) / achievementKey =>
-      userAchievementsRepo.deleteByKey(steamId, appId, achievementKey).transact(xa).attempt.flatMap {
+    case DELETE -> Root / LongVar(rawSteamid) / AchievementsURL / IntVar(appId) / achievementKey =>
+      val steamid: SteamId = SteamId(rawSteamid)
+      userAchievementsRepo.deleteByKey(steamid, appId, achievementKey).transact(xa).attempt.flatMap {
         case Right(rowsDeleted) if rowsDeleted > 0 =>
-          Ok(Map("message" -> s"Achievement $achievementKey removed for game $appId and user $steamId"))
+          Ok(Map("message" -> s"Achievement $achievementKey removed for game $appId and user $steamid"))
 
         case Right(_) =>
-          NotFound(Map("error" -> s"Achievement $achievementKey not found for game $appId and user $steamId"))
+          NotFound(Map("error" -> s"Achievement $achievementKey not found for game $appId and user $steamid"))
 
         case Left(error) =>
           InternalServerError(Map("error" -> s"Error deleting achievement $achievementKey: ${error.getMessage}"))
